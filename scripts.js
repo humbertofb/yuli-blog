83bdbf9f9877b2d1e0c11a61a84a11000b7f0757
humbertofb <98710153+humbertofb@users.noreply.github.com>
2024-08-08 23:07:44 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-// Asegúrate de que el DOM esté completamente cargado antes de ejecutar el código
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM completamente cargado");
+
     // Inicializar Firebase
     const firebaseConfig = {
         apiKey: "AIzaSyBRr5Q9VTe03Vo8PKSmher5r62W0n7Ej-k",
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         measurementId: "G-HNWTFFS4BP"
     };
 
-    // Inicializa Firebase
+    // Inicializar Firebase
     firebase.initializeApp(firebaseConfig);
     const db = firebase.firestore();
 
@@ -27,16 +28,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Manejar el envío de comentarios
     document.getElementById('commentForm').addEventListener('submit', function(e) {
-        e.preventDefault();
+        e.preventDefault();  // Prevenir el envío por defecto
+        console.log("Formulario enviado - preventDefault llamado");
 
         const username = document.getElementById('username').value;
         const comment = document.getElementById('comment').value;
+
+        if (!username || !comment) {
+            console.error("El nombre de usuario o comentario está vacío.");
+            return;
+        }
 
         db.collection('comments').add({
             username: username,
             comment: comment,
             timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }).then(() => {
+            console.log("Comentario guardado exitosamente");
             // Limpiar el formulario
             document.getElementById('commentForm').reset();
             loadComments(); // Recargar los comentarios
